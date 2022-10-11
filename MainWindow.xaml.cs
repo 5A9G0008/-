6268558d68598caf2ec54b31933079081ce042c5
5A9G0008 <82190564+5A9G0008@users.noreply.github.com>
2022-10-11 20:07:27 +0800

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace class1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MessageBoxButton buttons = MessageBoxButton.OKCancel;
+
+           MessageBox.Show("Hello WPF!","My first WPF program",buttons);
+        
+        }
+    }
+}
